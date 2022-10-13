3ae2f63a17149d62cb40d8ba11cd9cd676f691c9
lpdswing <lpdswing@qq.com>
2022-10-13 11:39:55 +0800

    配置config

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,7 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+    lang: 'zh-CN',
+    title: 'Interview Notes',
+    description: '技术面试基础知识, 算法, 网络, 操作系统, 数据库, Python'
+})

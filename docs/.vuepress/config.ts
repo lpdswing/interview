14aa071e5833ec41b697c0894cd533b029126e1b
lpdswing <lpdswing@qq.com>
2023-09-08 14:53:43 +0800

    add analytics for vercel

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -6,7 +6,10 @@ export default defineUserConfig({
     lang: 'zh-CN',
     title: 'Interview Notes',
     description: '技术面试基础知识, 算法, 网络, 操作系统, 数据库, Python',
-    head: [['link', {rel: 'shortcut icon', href: 'favicon.ico'}]],
+    head: [
+        ['link', {rel: 'shortcut icon', href: 'favicon.ico'}],
+        ['script', { src: "/_vercel/insights/script.js", defer: true}]
+    ],
     theme: defaultTheme({
         // Public 文件路径
         logo: 'favicon.ico',
